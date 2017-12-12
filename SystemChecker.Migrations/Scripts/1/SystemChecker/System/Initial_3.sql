7ca9d6c2749b9eeb8296667aaccbc2b7ca9f5597
Matt Jeanes <mattjeanes23@gmail.com>
2017-12-12 18:31:53 +0000

    Add database permission scripts

---
+++ SystemChecker.Migrations/Scripts/1/SystemChecker/System/Initial_3.sql
@@ -0,0 +1,9 @@
+﻿-- Adding datareader/datawriter roles
+
+DECLARE @user VARCHAR(255) = '***REMOVED***\***REMOVED***'
+
+EXEC sys.sp_addrolemember @rolename = 'db_datareader',  -- sysname
+                          @membername = @user -- sysname
+
+EXEC sys.sp_addrolemember @rolename = 'db_datawriter',  -- sysname
+                          @membername = @user -- sysname
