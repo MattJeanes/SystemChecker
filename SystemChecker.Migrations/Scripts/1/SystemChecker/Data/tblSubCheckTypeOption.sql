340e93c4dc87d7fe8e62c6bee4bcd1f35850fe02
Matt Jeanes <mattjeanes23@gmail.com>
2017-12-13 11:55:29 +0000

    Add script to fix db subcheck

---
+++ SystemChecker.Migrations/Scripts/1/SystemChecker/Data/tblSubCheckTypeOption.sql
@@ -0,0 +1,7 @@
+﻿IF ((SELECT Label FROM dbo.tblSubCheckTypeOption WHERE ID=7) = 'Column Name (JPath expression)') BEGIN
+	UPDATE dbo.tblSubCheckTypeOption SET Label='Field Name (JPath expression)' WHERE ID=7
+END
+
+IF ((SELECT OptionTypeID FROM dbo.tblSubCheckTypeOption WHERE ID=6) = 3) BEGIN
+	UPDATE dbo.tblSubCheckTypeOption SET OptionTypeID=2 WHERE ID=6
+END
