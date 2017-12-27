13115b2aa38a198c92b394d0c701cda8d470b442
raymond.bennett <raymond.bennett@moneybarn.com>
2017-12-27 14:24:34 +0000

    Added a 'Greater than' & a 'Less Than' subchecks for database checker.

---
+++ SystemChecker.Migrations/Scripts/3/SystemChecker/Data/tblSubCheckType.sql
@@ -0,0 +1,29 @@
+﻿IF NOT EXISTS(SELECT 1 FROM dbo.tblSubCheckType WHERE ID=5) BEGIN
+	SET IDENTITY_INSERT dbo.tblSubCheckType ON
+
+	INSERT INTO dbo.tblSubCheckType
+	(
+		ID,
+		CheckTypeID,
+		Name
+	)
+	VALUES
+	(   5, -- ID - int
+		2, -- CheckTypeID - int
+		'Field Greater Than' -- Name - varchar(255)
+	)
+
+	INSERT INTO dbo.tblSubCheckType
+	(
+		ID,
+		CheckTypeID,
+		Name
+	)
+	VALUES
+	(   6, -- ID - int
+		2, -- CheckTypeID - int
+		'Field Less Than' -- Name - varchar(255)
+	)
+
+	SET IDENTITY_INSERT dbo.tblSubCheckType OFF
+END
