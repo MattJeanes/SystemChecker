5563a973c010fdd1fe36053fbb6be9a38b6b61a2
Matt Jeanes <mattjeanes23@gmail.com>
2018-01-02 10:19:49 +0000

    Attempt memory cleanup of jobs

--- SystemChecker.Model/Helpers/JobFactory.cs
+++ SystemChecker.Model/Helpers/JobFactory.cs
@@ -5,24 +5,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Quartz;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SystemChecker.Model.Helpers
 {
     public class JobFactory : IJobFactory
     {
         private readonly IServiceProvider _container;
+        private Dictionary<IJob, IServiceScope> _scopes = new Dictionary<IJob, IServiceScope>();
         public JobFactory(IServiceProvider container)
         {
             _container = container;
         }
         public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
         {
-            return _container.GetService(bundle.JobDetail.JobType) as IJob;
+            var scope = _container.CreateScope();
+            var job = scope.ServiceProvider.GetRequiredService(bundle.JobDetail.JobType) as IJob;
+            _scopes[job] = scope;
+            return job;
         }
 
         public void ReturnJob(IJob job)
         {
             (job as IDisposable)?.Dispose();
+            _scopes[job]?.Dispose();
+            _scopes.Remove(job);
         }
     }
 }
