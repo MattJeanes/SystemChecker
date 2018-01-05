54d796c9357b591280726ef9622481949d4668e3
Matt Jeanes <mattjeanes23@gmail.com>
2018-01-05 15:57:14 +0000

    Add link to check from slack message

--- SystemChecker.Model/Notifiers/SlackNotifier.cs
+++ SystemChecker.Model/Notifiers/SlackNotifier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,13 +16,17 @@ namespace SystemChecker.Model.Notifiers
             ChannelId = 1
         }
         private readonly ISlackHelper _slackHelper;
-        public SlackNotifier(ICheckerUow uow, ISlackHelper slackHelper) : base(uow)
+        private readonly AppSettings _appSettings;
+        public SlackNotifier(ICheckerUow uow, ISlackHelper slackHelper, IOptions<AppSettings> appSettings) : base(uow)
         {
             _slackHelper = slackHelper;
+            _appSettings = appSettings.Value;
         }
 
         protected override async Task SendNotification(NotificationType type, string message, string failedAfter = null)
         {
+            var url = $"{_appSettings.Url}/details/{_check.ID}";
+            message = $"Check <{url}|{_check.Name}> completed in {_result.TimeMS}ms with result: {_result.Status.ToString()}";
             string channelID = _notification.Options[((int)Options.ChannelId).ToString()];
             _logger.Info($"Sending slack notification to channel ID {channelID}: {message}");
             await _slackHelper.SendMessage(channelID, message);
