d0942411b9c225adc80138d59152df25a9463226
Matt Jeanes <matthew.jeanes@moneybarn.com>
2018-02-12 17:55:35 +0000

    Log exceptions in job factory when creating job

--- SystemChecker.Model/Helpers/JobFactory.cs
+++ SystemChecker.Model/Helpers/JobFactory.cs
@@ -6,23 +6,34 @@ using System.Text;
 using System.Threading.Tasks;
 using Quartz;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SystemChecker.Model.Helpers
 {
     public class JobFactory : IJobFactory
     {
         private readonly IServiceProvider _container;
+        private readonly ILogger _logger;
         private Dictionary<IJob, IServiceScope> _scopes = new Dictionary<IJob, IServiceScope>();
-        public JobFactory(IServiceProvider container)
+        public JobFactory(IServiceProvider container, ILogger<JobFactory> logger)
         {
             _container = container;
+            _logger = logger;
         }
         public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
         {
-            var scope = _container.CreateScope();
-            var job = scope.ServiceProvider.GetRequiredService(bundle.JobDetail.JobType) as IJob;
-            _scopes[job] = scope;
-            return job;
+            try
+            {
+                var scope = _container.CreateScope();
+                var job = scope.ServiceProvider.GetRequiredService(bundle.JobDetail.JobType) as IJob;
+                _scopes[job] = scope;
+                return job;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Failed to create {bundle.JobDetail.JobType.Name} for {bundle.JobDetail.Key}");
+                throw;
+            }
         }
 
         public void ReturnJob(IJob job)
