f4ccfbf72c5c733ee9491397f41c6caec1cc2b4b
Matt Jeanes <matthew.jeanes@moneybarn.com>
2019-04-04 14:20:52 +0100

    Add dark theme, fixes #22230

--- SystemChecker.Web/ClientApp/src/app/app.component.ts
+++ SystemChecker.Web/ClientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { MessageService, PageService } from "./services";
 
@@ -7,6 +7,37 @@ import { MessageService, PageService } from "./services";
     templateUrl: "./app.template.html",
     styleUrls: ["./app.style.scss"],
 })
-export class AppComponent {
-    constructor(public messageService: MessageService, public pageService: PageService) { }
+export class AppComponent implements OnInit {
+   
+    get theme(): string { return localStorage.theme; }
+    set theme(value: string) { localStorage.theme = value; }
+
+    private activeTheme?: string;
+
+    constructor(
+        public messageService: MessageService,
+        public pageService: PageService,
+    ) { }
+
+    public ngOnInit() {
+        this.updateTheme(this.theme);
+    }
+
+    public switchTheme() {
+        if (!this.theme || this.theme === "light") {
+            this.theme = "dark";
+        } else {
+            this.theme = "light";
+        }
+        this.updateTheme(this.theme);
+    }
+
+    public updateTheme(theme: string) {
+        const htmlTag = document.getElementsByTagName("html")[0];
+        htmlTag.classList.add(theme);
+        if (this.activeTheme) {
+            htmlTag.classList.remove(this.activeTheme);
+        }
+        this.activeTheme = theme;
+    }
 }
